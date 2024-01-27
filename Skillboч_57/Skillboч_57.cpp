8511520c1e84e565afb4e254730cfd0389ac836b
SEGARS <vim_branet@mail.ru>
2024-01-27 15:24:34 +0500

    дз по массивам

--- Skillboч_57/Skillboч_57.cpp
+++ Skillboч_57/Skillboч_57.cpp
@@ -2,7 +2,13 @@
 #include <cassert>
 using namespace std;
 
-/**/
+/*Задание 3. Использование assert
+
+Что нужно сделать:
+Создайте в своей программе функцию float travelTime(float distance, float speed). 
+Она будет находить время в пути и при этом использовать assert, чтобы отбросить нулевую и отрицательную скорость. 
+Вызовите эту функцию в main. Убедитесь, что падение происходит за счёт assert. 
+Не забудьте подключить заголовочный файл cassert.*/
 
 float travelTime(float distance, float speed) 
 {
