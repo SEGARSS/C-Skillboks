4610b326d0ca813e96f599a15b4694c0d9513f80
SEGARSS <vim_branet@mail.ru>
2024-02-15 16:24:25 +0500

    dz *&

---
+++ Skillbox_86/Skillbox_86.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<cstring>
+using namespace std;
+
+/**/
+
+//--------------------------------------------------------------------------------------------------------
+bool substr(const char *str, const char *sub)
+{
+	return strstr(str, sub) != nullptr;
+}
+//--------------------------------------------------------------------------------------------------------
+int main()
+{
+	const char *a = "Hello world";
+	const char *b = "wor";
+	const char *c = "banana";
+
+	cout << boolalpha << substr(a, b) << " " << substr(a, c) << endl;
+
+	return 0;
+}
+//--------------------------------------------------------------------------------------------------------
