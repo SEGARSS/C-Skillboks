12f3ca2e3b669ff4f7b684261c2b39c4fedd0414
SEGARSS <vim_branet@mail.ru>
2024-02-19 16:08:14 +0500

    дз по работе с файлами

---
+++ Skillbox_90/Skillbox_90.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+#include <unordered_map>
+using namespace std;
+
+/*Задание 1. Разработка программы поиска слов в файле
+Что нужно сделать
+Составьте небольшой текстовый файл «words.txt», в котором последовательно, 
+через пробелы и переносы строк расположены различные слова. Можете использовать любые англоязычные слова. 
+Разработайте программу, которая искала бы среди этих слов нужное пользователю и выводила бы общее количество повторений этого слова. 
+0 — если слово вообще не присутствует в файле.*/
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	string path = "words.txt"; 
+
+	ofstream fout; 
+
+	fout.open(path, ofstream::app); 
+
+	fout.close();
+
+	ifstream fin; // Команда считывания файла 
+	fin.open(path); // Вывод на консоль
+
+	if ( !fin.is_open() ) // Вывод на консоль
+	{
+		cout << "Ошибка открытия файла! " << endl;
+	}
+	else
+	{
+		cout << "Файл открыт! " << endl;
+	}
+
+	unordered_map<string, int> wordCount;
+
+	string word;
+	while (fin >> word) 
+	{
+		wordCount[word]++;
+	}
+
+	fin.close();
+
+	cout << "Введите слово для поиска: ";
+   string searchWord;
+   cin >> searchWord;
+    
+	// Поиск и вывод количества повторений слова
+	if (wordCount.find(searchWord) != wordCount.end())
+	{
+		cout << "Количество повторений слова \"" << searchWord << "\": " << wordCount[searchWord] << endl;
+	}
+	else
+	{
+		cout << "Слово \"" << searchWord << "\" не найдено в файле." << endl;
+	}
+	
+	return 0;
+}
---
+++ Skillbox_92/Skillbox_92.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+/*Задание 3. Реализация программы чтения ведомости
+Что нужно сделать
+Реализуйте программу чтения информации из ведомости по учёту выплат. Сама ведомость представляет собой следующую таблицу:
+• имя и фамилия человека;
+• количество денег, которые были выплачены лицу;
+• дата выплаты в формате ДД.ММ.ГГГГ.*/
+
+//-----------------------------------------------------------------------------------
+struct Payment
+{
+	string fullName;
+	string sorrName;
+	double amount;
+	string date;
+};
+//-----------------------------------------------------------------------------------
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	ifstream file("payments.txt");
+
+	if (!file.is_open())
+	{
+		cout << "Ошибка открытия файла." << endl;
+		return 1;
+	}
+
+	// Вывод содержимого файла
+	cout << "Содержимое ведомости:" << endl;
+	cout << "---------------------" << endl;
+	string line;
+	while (getline(file, line))
+	{
+		cout << line << endl;
+	}
+	cout << "---------------------" << endl;
+	cout << endl;
+
+	// Второй проход для анализа данных и вывода результата
+	file.clear();           // Сброс флагов ошибок файла
+	file.seekg(0, ios::beg); // Возврат к началу файла
+
+	double totalPayments = 0.0;
+	Payment maxPayment;
+	bool isFirstPayment = true;
+
+	while (getline(file, line))
+	{
+		istringstream iss(line);
+		Payment payment;
+		iss >> payment.fullName >> payment.sorrName >> payment.amount >> payment.date;
+
+		totalPayments += payment.amount;
+
+		if (isFirstPayment || payment.amount > maxPayment.amount)
+		{
+			maxPayment = payment;
+			isFirstPayment = false;
+		}
+	}
+
+	file.close();
+
+	cout << "Общая сумма выплат: $" << fixed << setprecision(2) << totalPayments << endl;
+	cout << "Человек с максимальной суммой выплат:" << endl;
+	cout << "Имя и фамилия: " << maxPayment.fullName << " " << maxPayment.sorrName << endl;
+	cout << "Сумма выплат: $" << fixed << setprecision(2) << maxPayment.amount << endl;
+	cout << "Дата выплаты: " << maxPayment.date << endl;
+
+	return 0;
+}
+//-----------------------------------------------------------------------------------
