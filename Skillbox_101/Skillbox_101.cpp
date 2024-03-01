70cc1e54df0e88988521940dc10330ce479ad7ef
SEGARSS <vim_branet@mail.ru>
2024-03-01 16:36:33 +0500

    дз Урок 4. Словари std::map

---
+++ Skillbox_101/Skillbox_101.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	map<string, string> phonebook;
+
+	cout << "Добро пожаловать в телефонный справочник!" << endl;
+
+	while (true) 
+	{
+		cout << "\nВыберите действие:" << endl;
+		cout << "1. Добавить номер и абонента" << endl;
+		cout << "2. Узнать фамилию абонента по номеру телефона" << endl;
+		cout << "3. Узнать номера телефонов по фамилии абонента" << endl;
+		cout << "4. Выйти из программы" << endl;
+
+		int choice;
+		cout << "Ваш выбор: ";
+		cin >> choice;
+
+		if (choice == 1) 
+		{
+			string lastName, phoneNumber;
+			cout << "Введите фамилию абонента: ";
+			cin >> lastName;
+			cout << "Введите номер телефона: ";
+			cin >> phoneNumber;
+			phonebook[phoneNumber] = lastName;
+			cout << "Номер и абонент успешно добавлены в справочник." << endl;
+		}
+		else if (choice == 2) 
+		{
+			string phoneNumber;
+			cout << "Введите номер телефона: ";
+			cin >> phoneNumber;
+
+			if (phonebook.find(phoneNumber) != phonebook.end() ) 
+			{
+				cout << "Фамилия абонента: " << phonebook[phoneNumber] << endl;
+			}
+			else 
+			{
+				cout << "Номер телефона не найден в справочнике." << endl;
+			}
+		}
+		else if (choice == 3) 
+		{
+			string lastName;
+			cout << "Введите фамилию абонента: ";
+			cin >> lastName;
+
+			bool found = false;
+
+			for (const auto& entry : phonebook) 
+			{
+				if (entry.second == lastName) 
+				{
+					cout << "Номер телефона: " << entry.first << endl;
+					found = true;
+				}
+			}
+			if (!found) 
+			{
+				cout << "Абонент с указанной фамилией не найден в справочнике." << endl;
+			}
+		}
+		else if (choice == 4) 
+		{
+			cout << "Спасибо за использование телефонного справочника. До свидания!" << endl;
+			break;
+		}
+		else 
+		{
+			cout << "Неверный выбор. Пожалуйста, попробуйте снова." << endl;
+		}
+	}
+
+	return 0;
+}
---
+++ Skillbox_102/Skillbox_102.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+
+
+using namespace std;
+
+/**/
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	map<string, int> patients;
+	string input;
+
+	cout << "Добро пожаловать в регистратуру!" << endl;
+
+	while (true)
+	{
+		cout << "Введите фамилию пациента или 'Next' для вызова следующего: ";
+		cin >> input;
+
+		if (input == "Next")
+		{
+			if (patients.empty() )
+			{
+				cout << "Очередь пустая." << endl;
+			}
+			else
+			{
+				auto it = patients.begin();
+				cout << "" << it->first << " приглашен." << endl;
+				it->second--;
+				if (it->second == 0)
+				{
+					patients.erase(it);
+				}
+			}
+		}
+		else
+		{
+			patients[input]++;
+			cout << "Пациент " << input << " добавлен в очередь." << endl;
+		}
+	}
+
+	return 0;
+}
