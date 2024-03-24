976fa19f94594ea43d778e2f22903946878d423a
SEGARS <vim_branet@mail.ru>
2024-03-24 13:54:38 +0500

    16.6 Практическая работа

---
+++ Skillbox_124/Skillbox_124.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	string buffer;
+	cout << "Введите выражение в формате <число1><действие><число2>: ";
+	getline(cin, buffer);
+
+	double a, b;
+	char operation;
+
+	stringstream buffer_stream(buffer);
+	buffer_stream >> a >> operation >> b;
+
+	double result;
+
+	switch (operation) 
+	{
+	case '+':
+		result = a + b;
+		break;
+	case '-':
+		result = a - b;
+		break;
+	case '*':
+		result = a * b;
+		break;
+	case '/':
+		if (b == 0) {
+			cout << "Ошибка: деление на ноль!" << endl;
+			return 1;
+		}
+		result = a / b;
+		break;
+	default:
+		cout << "Ошибка: неверный оператор!" << endl;
+		return 1;
+	}
+
+	cout << "Результат: " << result << endl;
+
+	return 0;
+}
